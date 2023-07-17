588f9a7ecbf8ee7f71689a9aaf3c0ecfe30fa60b
Raymond <rayng86@gmail.com>
2023-07-17 01:05:22 -0400

    feat: add basic query to fetch my git username

--- pr-analysis.py
+++ pr-analysis.py
@@ -4,3 +4,17 @@ load_dotenv()
 
 github_graphql_api_url = os.getenv('GITHUB_GRAPHQL_API_URL')
 access_token = os.getenv('ACCESS_TOKEN')
+query = '''
+query {
+  repository(owner: \"rayng86\", name: \"ray_test_repo\") {
+    pullRequests(states: MERGED, first: 50, orderBy: {field: UPDATED_AT, direction: DESC}) {
+      nodes {
+        author {
+          login
+        }
+      }
+    }
+  }
+}
+'''
+
